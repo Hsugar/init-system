9b496c84029b0a95e308a633276126a16af3f1ec
DESKTOP-3IUMQ69\wenfeng <yejiani@lexue.com>
2019-01-24 17:18:59 +0800

    '实现导航的面包屑功能'

--- src/components/Position.vue
+++ src/components/Position.vue
@@ -1,70 +1,73 @@
 <template>
-  <div class="position ">
-    <!-- <router-link to="/">{{home}}</router-link>/
-    <slot></slot> -->
-    <div class="level-bread">
-      <el-breadcrumb separator=" / ">
-        <el-breadcrumb-item v-for="item in realList" :key="item.id" :to="item.path">{{item.title}}</el-breadcrumb-item>
-      </el-breadcrumb>
-    </div>
-  </div>
+	<div class="position">
+		
+		<div class="level-bread">
+			<el-breadcrumb separator=" / ">
+				<el-breadcrumb-item v-for="item in realList" :key="item.id" :to="item.path">{{item.title}}</el-breadcrumb-item>
+			</el-breadcrumb>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "Position",
-  methods: {},
-  data() {
-    return {
-      home: "首页",
-      realList: []
-    };
-  },
-  components: {},
-  watch: {
-    $route: function(newV, oldV) {
-      this.realList = newV.meta.routeList;
-      console.log(this.realList);
-    }
-  },
-  created() {
-    this.getRoutePath();
-  },
-  methods: {
-    getRoutePath() {
-      this.realList = this.$route.meta.routeList;
-      // getBreadcrumb() {
-      // 	var breadNumber= this.$route.query.breadNum || 1;//url变量breadNum记录层级，默认为1，如果大于1，要添加上变量；
-      // 	var breadLength=this.$store.state.breadListState.length;//目前breadlist集合数组个数
-      // 	var curName=this.$route.name;
-      // 	var curPath=this.$route.fullPath;
-      // 	var newBread={name:curName,path:curPath};
-      // 	var ishome=curName=='首页';
-      // 	console.log(ishome);
-      // 	if(breadNumber===1){//点击一级菜单
-      // 	this.$store.commit('breadListStateRemove',1);//初始化，只有首页面包屑按钮
-      // 	if(!ishome)//如果不是首页
-      // 		this.$store.commit('breadListStateAdd',newBread);//当前页面添加到breadlist集合
-      // 	}
-      // 	else if(breadLength<=breadNumber){//如果不是一级导航，并且breadlist集合个数等于或者小于目前层级
-      // 	this.$store.commit('breadListStateAdd',newBread);//要把当前路由添加到breadlist集合
-      // 	}else{
-      // 	this.$store.commit('breadListStateRemove',parseInt(breadNumber)+1);//如果往回点面包屑导航，截取；
-      // 	}
-      // 	this.breadlist=this.$store.state.breadListState;
-      // 	console.log(this.breadlist);
-      // }
-    }
-  }
+	name: "Position",
+	data() {
+		return {
+			home: "系统设置",
+			realList: []
+		};
+	},
+	components: {},
+	created() {
+		this.getRoutePath();
+	},
+	methods: {
+		getRoutePath() {
+			//拆分当前路由地址，截取并拼接父级路由地址。再在路由表中查询对应的项目，获取面包屑导航信息
+			let paths = '';
+			let pathArr = this.$route.path.split('/');
+			console.log('patharr==',this.$route.path,pathArr)
+			pathArr.shift();
+			this.realList = pathArr.map(p => { 
+				paths += `/${p}`;	//重新拼接路径中的每一级
+				const node = this.$router.options.routes.filter((m) => m.path === paths)[0];	//从路由表中获取每一级对应的路由信息
+				console.log('node==',node)
+				return {title: node.meta.title, path: node.path};
+			
+			})
+		}
+	}
 };
 </script>
 <style lang="scss" scoped>
-.position{
-  padding: 18px 26px;
+.position {
+	padding: 18px 26px;
+  font-size: 15px;
+  a{
+    color: #393d4a;
+    cursor: pointer;
+    margin-left: 12px;
+    font-weight: bold;
+  }
+	.level-bread {
+		position: relative;
+		&:after {
+			content: "";
+			display: inline-block;
+			width: 3px;
+			height: 14px;
+			background: #4098f7;
+			position: absolute;
+			top: 0;
+			left: 0;
+		}
+	}
 }
-.el-breadcrumb__item{
+.el-breadcrumb__item {
 	color: #999;
-	cursor: pointer;
+  cursor: pointer;
+  margin-left: 8px;
 }
 </style>
 
