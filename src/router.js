9e7642c0d9e6b85c8269438ef21f2ec96d62cf26
DESKTOP-3IUMQ69\wenfeng <yejiani@lexue.com>
2019-01-25 18:24:49 +0800

    '更新router，改二级路由为一级路由'

--- src/router.js
+++ src/router.js
@@ -3,62 +3,63 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
-
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
-      meta: {title: '首页'},
+      meta: {
+        title: '系统设置'
+      },
       component: Home
     },
     {
       path: '/home',
       name: 'home',
-      meta: {title: '首页'},
+      meta: {
+        title: '系统设置'
+      },
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      meta: {title: '关于'},
+      path: '/record',
+      name: 'school',
+      meta: {
+        title: '基础档案'
+      },
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import( /* webpackChunkName: "about" */ './views/record/school/index.vue'),
+      redirect: '/record/school'
     },
     {
-      path: '/record',
-      name: 'record',
-      meta: {title: '基础档案'},
+      path: '/record/school',
+      name: 'school',
+      meta: {
+        title: '学校档案'
+      },
       component: () => import('./views/record/school.vue'),
-      children: [
-        {
-          path: '/record',
-          name: 'record',
-          meta: {title: '校区档案'},
-          component: () => import('./views/record/school.vue'),
-        },
-        {
-          path: '/record/student',
-          name: 'student',
-          meta: { title: '学生档案'},
-          component: () => import('./views/record/student.vue'),
-        },
-        {
-          path: '/record/teacher',
-          name: 'teacher',
-          meta: { title: '教师档案'},
-          component: () => import('./views/record/teacher.vue'),
-        },
-       
-      ]
     },
-    // {
-    //   path: '/record/student',
-    //   name: 'student',
-    //   meta: { title: '学生档案'},
-    //   component: () => import('./views/record/student.vue'),
-    // },
+    {
+      path: '/record/campus',
+      name: 'campus',
+      meta: {
+        title: '校区档案'
+      },
+      component: () => import('./views/record/campus.vue'),
+    },
+    {
+      path: '/record/season',
+      name: 'season',
+      meta: {
+        title: '学生档案'
+      },
+      component: () => import('./views/record/student.vue'),
+    },
+   
+   
+   
+   
+   
   ]
 })
